2bcd1a85e4c02a0958fddcd28f41f07db54bdf9c
Karl Kosack <karl.kosack@cea.fr>
2025-01-31 19:41:17 +0100

    added some metadata as examples

--- src/vodf_schema/level1/eventlist.py
+++ src/vodf_schema/level1/eventlist.py
@@ -30,8 +30,26 @@ class EventList(BinaryTable):
         )
 
     # Mandatory Columns
-    EVENT_ID = Int64()
-    TIME = Double(unit=u.s)
-    RA = Double(unit=u.deg)
-    DEC = Double(unit=u.deg)
-    ENERGY = Double(unit=u.TeV)
+    EVENT_ID = Int64(
+        description="ID of this event, unique within an observation",
+    )
+    TIME = Double(
+        description="Event time of arrival, as an MET",
+        unit=u.s,
+        ucd="time",
+    )
+    RA = Double(
+        description="Reconstructed Right Ascension of event point-of-origin.",
+        unit=u.deg,
+        ucd="pos.eq.ra;stat.fit",
+    )
+    DEC = Double(
+        description="Reconstructed Declination of event point-of-origin",
+        unit=u.deg,
+        ucd="pos.eq.dec;stat.fit",
+    )
+    ENERGY = Double(
+        description="Reconstructed event energy",
+        unit=u.TeV,
+        ucd="phys.energy;stat.fit",
+    )
